8a84c1d3d9e9c2cc07d4386982cf2bd1f6ae4def
ZFX <2675699284@qq.com>
2019-11-15 11:21:44 +0800

    Update zfx.bat

--- img/zy/zfx.bat
+++ img/zy/zfx.bat
@@ -46,124 +46,153 @@
 @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedgstf.txt
 @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedfgdfstf.txt
 @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegdfgstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefdgdfggstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedfgsdsdggstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdgestf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessgsdgtf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsfgssstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfgsdstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesssdfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsefstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessfssstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefssfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfssfsfsftf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfhsfsfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\thsshestf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\hhsg.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesgrtergrtf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesergwergergtf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegergstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teerrgstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfghttf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teergerstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\terggerstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegfrdrstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegfwestf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegergstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teswefweffsdftf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesefstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsfsetf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdfftf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefeestf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewefwefwfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewefwefdsstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teefstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedfsdfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdsdfstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfsdstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefefstf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdfwetf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te453453stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te345stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes433434tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes433tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes434555tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te543stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te3453453stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te1531stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t1es312tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t12estf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te112stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te13stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te233stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\23452452.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t2532estf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t4535estf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes33tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te535stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te535stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te54344stf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes4545tf.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\terffst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdffsdt.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdfest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedsfsdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefwefwefsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdfsdfsdest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsfsdest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdfest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\4948.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedsfwefst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tffsafest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdft.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesast.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasddfst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdadaast.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teadadst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdaddfsdsdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdffsdfsdest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsddfsasst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfdasst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasddst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\fgvdyfg.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tgrtfgergest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegregerst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegddgest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tgegeest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdgdtgwdrsest.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedgdrgsdtrst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teestfgerst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teertgergst.txt
-echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teertgergst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teertgergst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefdgdfggstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedfgsdsdggstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdgestf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessgsdgtf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsvfgssstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfgsdstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesssdfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsefstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessfssstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefssfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfssfsfsftf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfhsfsfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\thsshestf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\hhsg.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesgrtergrtf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesergwergergtf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegergstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teerrgstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfghttf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teergerstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\terggerstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegfrdrstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegfwestf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegergstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teswefweffsdftf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesefstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsfsetf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdfftf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefeestf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewefwefwfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tewefwefdsstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teefstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedfsdfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdsdfstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfsdstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefefstf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdfwetf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te453453stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te345stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes433434tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes433tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes434555tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te543stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te3453453stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te1531stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t1es312tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t12estf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te112stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te13stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te233stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\23452452.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t2532estf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\t4535estf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes33tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te535stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te535stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\te54344stf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tes4545tf.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\terffst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdffsdt.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdfest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedsfsdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfsst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefwefwefsst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teffsdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdfsdfsdest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsfsdest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdfsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tfsdfest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsdst.txt
+@start cmd | @start cmd | @start cmd | @start cmd | @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\4948.txt
+@start cmd | @start cmd | @start cmd | @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdfsst.txt
+@start cmd | @start cmd | @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedsfwefst.txt
+@start cmd | @start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tffsafest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tessdft.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesfsst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesast.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasddfst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tesdadaast.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teadadst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdaddfsdsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdaddfsdsdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdffsdfsdest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tefsddfsasst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdfdasst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasddst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdasst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teasdst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\fgvdyfg.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tgrtfgergest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegregerst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tegddgest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tgegeest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tsdgdtgwdrsest.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\tedgdrgsdtrst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teestfgerst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teertgergst.txt
+@start cmd | echo ^^W ^> ^W>C:\Users\Administrator\Desktop\teertgergst.txt
 md ripgipr 8er89 e  tuu890t890gueriogererererererererererg  ry hrt grt g  gdrghf h rt h rt h rth rth fgbrtf  rtfh rth rt hthrth rt h rthrt h rt h r h rth rth rt hrthrt h rt h rt hrt h rth rt h rth rt hrj ry j rj h j y j grd jhe56u w35r tgewer sdg rtduy34er tgdf h r6th f    54 64 5614 56156464564 56 4564 564 4564 4 56 4 564 4 56 456 456  564 564 564 56 456 456 4 564 564 4564 89 56 57 894  56 4 456 194 84 9 564 89 4
+@start cmd | color 1
+@start cmd | color 2
+@start cmd | color 3
+@start cmd | color 4
+@start cmd | color 5
+@start cmd | color 6
+@start cmd | color a
+@start cmd | color b
+@start cmd | color c
+@start cmd | color d
+@start cmd | color e
+@start cmd | color f
+@start cmd | color 1a
+@start cmd | color 1b
+@start cmd | color 1c 
+@start cmd | color 1d
+@start cmd | color 1f
+@start cmd | color 1e
+@start cmd | color 2a
+@start cmd | color 2b
+@start cmd | color 2c
+@start cmd | color 2d
+@start cmd | color 2e
+@start cmd | color 2f
+@start cmd | color 3a
+@start cmd | color 3b
+@start cmd | color 3c
 %0
